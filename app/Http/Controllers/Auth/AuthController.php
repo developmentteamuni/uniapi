fd0507d8daa7d65cae87d78cb5fdc5f861cef22d
Precious Oladele <oladelep77@gmail.com>
2022-09-05 10:13:43 -0700

    sqlite

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
         {
             return response([
                 'message' => 'Account has already been verified'
-            ]);
+            ], 400);
         }else{
 
         $user = User::where([['email', $request->email], ['otp', $request->otp]])->first();
