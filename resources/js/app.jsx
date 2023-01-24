c79cf647d2b902a6ceb0e0521ab78a8c43b39178
JeremyWebb45 <jwebb45@gatech.edu>
2023-01-23 19:53:43 -0500

    Clean up dev environment

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,8 +5,8 @@ import { createRoot } from "react-dom/client";
 
 createInertiaApp({
     resolve: (name) => {
-        const pages = import.meta.glob("../pages/**/*.jsx", { eager: true });
-        return pages[`../pages/${name}.jsx`];
+        const pages = import.meta.glob("./pages/**/*.jsx", { eager: true });
+        return pages[`./pages/${name}.jsx`];
     },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
@@ -8,6 +7,5 @@ export default defineConfig({
             input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
-        react(),
     ],
 });
