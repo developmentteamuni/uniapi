3ce9e18e587e23d8ef2213fce9a9384cb9d74a86
Precious Oladele <Oladelep77@gmail.com>
2023-03-25 17:08:24 +0100

    [update] Profile Resources with Image Width and Height

--- app/Http/Resources/ProfileController.php
+++ app/Http/Resources/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+// use Intervention\Image\ImageManagerStatic as Image;
 
 class ProfileController extends JsonResource
 {
@@ -19,12 +20,14 @@ class ProfileController extends JsonResource
             'user_id' => $this->user_id,
             'fullname' => $this->user->firstname . ' ' . $this->user->lastname,
             'body' => $this->body,
-            'image' => $this->image != null ? env('APP_URL') . '/public/feedImages/' . $this->image : null,
+            'image' => $this->image != null ? env('APP_URL') . 'public/feedImages/' . $this->image : null,
             'created_at' => $this->created_at,
             'reacted' => $this->reacted,
             'saved' => $this->saved,
             'profileImg' => $this->user->profile->profileImg ?? 'https://ui-avatars.com/api/?name=' . $this->user->firstname . '+' . $this->user->lastname,
             'liked_by' => $this->likedby,
+            'width' => $this->image != null ? getimagesize('public/feedImages/' . $this->image)[0] : null,
+            'height' => $this->image != null ? getimagesize('public/feedImages/' . $this->image)[1] : null,
         ];
     }
 }
